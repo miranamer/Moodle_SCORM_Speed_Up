794895155829baabf86ea0d14895af16618ac433
Miran-Amer <thesweatyog123@gmail.com>
2024-04-03 15:28:27 +0100

    Added LocalStorage Removal

--- service_worker.js
+++ service_worker.js
@@ -2,6 +2,46 @@ function saveTabId(tabId) {
   chrome.storage.local.set({ savedTabId: tabId });
 }
 
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+  // Check if savedTabId exists in local storage
+
+  chrome.storage.local.get(null, function (data) {
+    if (chrome.runtime.lastError) {
+      console.error(
+        "Error retrieving data from local storage:",
+        chrome.runtime.lastError.message
+      );
+    } else {
+      console.log("Data in local storage before removal:", data);
+    }
+  });
+
+  chrome.storage.local.get("savedTabId", function (data) {
+    if (chrome.runtime.lastError) {
+      console.error(
+        "Error retrieving savedTabId from local storage:",
+        chrome.runtime.lastError.message
+      );
+    } else {
+      if ("savedTabId" in data) {
+        // savedTabId exists, remove it
+        chrome.storage.local.remove("savedTabId", function () {
+          if (chrome.runtime.lastError) {
+            console.error(
+              "Error removing savedTabId from local storage:",
+              chrome.runtime.lastError.message
+            );
+          } else {
+            console.log("savedTabId removed from local storage.");
+          }
+        });
+      } else {
+        console.log("savedTabId not found in local storage, no action taken.");
+      }
+    }
+  });
+});
+
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
     if (details.url.endsWith("index_lms.html")) {
