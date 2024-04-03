7603d77b7bca3b5845c1961871e87eb00570c8f5
Miran-Amer <thesweatyog123@gmail.com>
2024-04-03 14:49:05 +0100

    Final Extension

--- popup.js
+++ popup.js
@@ -7,24 +7,47 @@ function getSavedTabId() {
 }
 
 document.addEventListener("DOMContentLoaded", async function () {
-  var addBorderButton = document.getElementById("runCode");
+  var addBorderButton125 = document.getElementById("runCode125");
+  var addBorderButton15 = document.getElementById("runCode15");
+  var addBorderButton2 = document.getElementById("runCode2");
   const tabId = await getSavedTabId();
-  addBorderButton.addEventListener("click", function () {
+  addBorderButton125.addEventListener("click", function () {
     chrome.scripting
         .executeScript({
           target: { tabId: tabId, allFrames: true },
           func: increasePlaybackSpeedFunc,
+          args: [1.25]
+        })
+        .then(() => console.log("script injected in tabId", tabId));
+  });
+
+  addBorderButton15.addEventListener("click", function () {
+    chrome.scripting
+        .executeScript({
+          target: { tabId: tabId, allFrames: true },
+          func: increasePlaybackSpeedFunc,
+          args: [1.5]
+        })
+        .then(() => console.log("script injected in tabId", tabId));
+  });
+
+  addBorderButton2.addEventListener("click", function () {
+    chrome.scripting
+        .executeScript({
+          target: { tabId: tabId, allFrames: true },
+          func: increasePlaybackSpeedFunc,
+          args: [2]
         })
         .then(() => console.log("script injected in tabId", tabId));
   });
 });
 
-const increasePlaybackSpeedFunc = () => {
+const increasePlaybackSpeedFunc = (speed) => {
   console.log(document);
   for (let i = 0; i < document.querySelectorAll("video").length; i++) {
     if (document.querySelectorAll("video")[i].src.includes(".php")) {
       console.log("Y", document.querySelectorAll("video")[i]);
-      document.querySelectorAll("video")[i].playbackRate = 2;
+      document.querySelectorAll("video")[i].playbackRate = speed;
     }
   }
 };
